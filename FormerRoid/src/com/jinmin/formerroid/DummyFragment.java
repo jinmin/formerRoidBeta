bbaceeafc130c731b6dbcf3fecae378c0753bdd9
mwjin <mwjin0312@gmail.com>
2013-05-08 17:34:07 +0900

    ddd

--- FormerRoid/src/com/jinmin/formerroid/DummyFragment.java
+++ FormerRoid/src/com/jinmin/formerroid/DummyFragment.java
@@ -1,26 +1,28 @@
 package com.jinmin.formerroid;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class DummyFragment extends Fragment {
+public class DummyFragment extends Fragment
+{
+	public static final String ARG_SECTION_NUMBER = "section_number";
 
-	public DummyFragment() {
-    }
+	public DummyFragment()
+	{
+	}
 
-    public static final String ARG_SECTION_NUMBER = "section_number";
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		TextView textView = new TextView(getActivity());
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-       TextView textView = new TextView(getActivity());
-       textView.setGravity(Gravity.CENTER);
-       Bundle args = getArguments();
-       textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
-       return textView;
-    }
+		Bundle args = getArguments();
+		textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
+		return textView;
+	}
+
 }
